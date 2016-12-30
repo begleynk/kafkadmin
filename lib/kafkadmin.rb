6843f5646c820b41cb26f248c7724d46249cc4d3
Niklas Begley <niklas.begley@gmail.com>
2016-12-30 12:41:23 +0000

    Added ability to daemonize Kafkadmin

--- lib/kafkadmin.rb
+++ lib/kafkadmin.rb
@@ -21,6 +21,23 @@ module Kafkadmin
   end
 
   def self.start_server!
+    # Run as a daemon process if we pass --daemon
+    if config.fetch(:daemon)
+      puts "Starting Kafkadmin daemon..."
+      Process.daemon(true)
+    end
+
     Web.run!
+  end
+
+  def self.stop_server!
+    pid = `pgrep -f kafkadmin`.chomp
+
+    if pid =~ /\d+/
+      puts "Killing Kafkadmin at PID #{pid}..."
+      Process.kill('INT', pid.to_i)
+    else
+      puts 'Could not find a running Kafkadmin server.'
+    end
   end
 end
